8dc9a67545645423cf3d1f9b8684045eba7a2952
jack-linden <jkl2693@gmail.com>
2015-06-09 18:09:51 -0400

    Working Skeleton
    This commit is for our working skeleton. It contains one model object
    (DogRecord) and one service (RecordRetreivalService). It also contains a
    test class for RecordRetrievalService with two passing negative tests.

---
+++ test/services/RecordRetrievalServiceTest.java
@@ -0,0 +1,48 @@
+package services;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import model.DogRecord;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecordRetrievalServiceTest {
+
+	public final String NON_EXISTENT_DOG_NAME = "LD@()";
+	public final String DOG_NAME_A = "Buddy";
+	
+	public RecordRetrievalService recordService;
+	
+	@Before
+	public void prepareTests( ){
+		recordService = new RecordRetrievalService();
+	}
+	
+	
+	/*
+	 *  TC1.queryRecordsByName
+	 *  
+	 *  This test queries the database with a null argument for name. 
+	 *  Expects an IllegalArgumentException to be thrown.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void queryRecordsByNameNullParameterTest( ){
+		recordService.queryRecordsByName(null);
+	}
+	
+	/*
+	 *  TC2.queryRecordsByName
+	 *  
+	 *  This test queries the database with a non-existent name. 
+	 *  Expects to receive an empty list of dog records.
+	 */
+	@Test
+	public void queryRecordsByNameNonExistentRecordTest( ){
+		
+		List<DogRecord> dogRecords = recordService.queryRecordsByName(NON_EXISTENT_DOG_NAME);
+		assertTrue(dogRecords.isEmpty());
+	}
+}
