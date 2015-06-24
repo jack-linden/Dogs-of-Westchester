58f9758e2b229837b83650f8419fb7b2e5fbdeaf
Jiayan Zhang <jiz86@pitt.edu>
2015-06-23 20:05:03 -0400

    fix formatter

--- src/test/java/services/RecordRetrievalServiceTest.java
+++ src/test/java/services/RecordRetrievalServiceTest.java
@@ -10,35 +10,34 @@ public class RecordRetrievalServiceTest {
 
 	public final String NON_EXISTENT_DOG_NAME = "LD@()";
 	public final String DOG_NAME_A = "Buddy";
-	
+
 	public RecordRetrievalService recordService;
-	
+
 	@Before
-	public void prepareTests( ){
+	public void prepareTests() {
 		recordService = new RecordRetrievalService();
 	}
-	
-	
+
 	/*
-	 *  TC1.queryRecordsByName
-	 *  
-	 *  This test queries the database with a null argument for name. 
-	 *  Expects an IllegalArgumentException to be thrown.
+	 * TC1.queryRecordsByName
+	 * 
+	 * This test queries the database with a null argument for name. Expects an
+	 * IllegalArgumentException to be thrown.
 	 */
-	@Test(expected=IllegalArgumentException.class)
-	public void queryRecordsByNameNullParameterTest( ){
+	@Test(expected = IllegalArgumentException.class)
+	public void queryRecordsByNameNullParameterTest() {
 		recordService.queryRecordsByName(null);
 	}
-	
+
 	/*
-	 *  TC2.queryRecordsByName
-	 *  
-	 *  This test queries the database with a non-existent name. 
-	 *  Expects to receive an empty list of dog records.
+	 * TC2.queryRecordsByName
+	 * 
+	 * This test queries the database with a non-existent name. Expects to
+	 * receive an empty list of dog records.
 	 */
 	@Test
-	public void queryRecordsByNameNonExistentRecordTest( ){
-		
+	public void queryRecordsByNameNonExistentRecordTest() {
+
 		List<Dog> dogRecords = recordService.queryRecordsByName(NON_EXISTENT_DOG_NAME);
 		assertTrue(dogRecords.isEmpty());
 	}
