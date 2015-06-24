d028d3c6cdf015c66c6bc083308bd90b83344770
Jiayan Zhang <jiz86@pitt.edu>
2015-06-23 20:14:29 -0400

    no message

--- src/main/java/model/Dog.java
+++ src/main/java/model/Dog.java
@@ -1,38 +1,45 @@
 package model;
 
 public class Dog {
-	
+
 	private int idNumber;
 	private String name;
 	private String breed;
 	private String location;
-	
-	/* 
+
+	/*
 	 * Getters and Setters for DogRecord fields.
 	 */
 	public int getIdNumber() {
 		return idNumber;
 	}
+
 	public void setIdNumber(int idNumber) {
 		this.idNumber = idNumber;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getBreed() {
 		return breed;
 	}
+
 	public void setBreed(String breed) {
 		this.breed = breed;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
-		
+
 }
