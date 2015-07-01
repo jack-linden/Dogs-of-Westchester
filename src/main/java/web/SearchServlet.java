171f28d1228da76e09afadd0e69633c788d04956
jack-linden <jkl2693@gmail.com>
2015-07-01 17:46:38 -0400

    Updated index.html (home page) with a functional search bar and search results table. Used AJAX requests to avoid page refreshes. Created SearchServlet.java to handle GET requests for search queries.

---
+++ src/main/java/web/SearchServlet.java
@@ -0,0 +1,44 @@
+package web;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import model.Dog;
+import services.RecordRetrievalService;
+
+public class SearchServlet extends HttpServlet {
+
+	public final Gson gson = new Gson();
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("application/json");
+		RecordRetrievalService recordService = new RecordRetrievalService();
+		String queryText = req.getParameter("search-text");
+		
+		Type collectionType = new TypeToken<List<String>>(){}.getType();
+		List<String> queryProperties = gson.fromJson(req.getParameter("search-properties"), collectionType);
+		
+		Set<Dog> dogs = new HashSet<Dog>();
+		for (String property : queryProperties) {
+			dogs.addAll(recordService.queryDogRecords(property, queryText, false));
+		}
+		
+		String jsonDogsString = gson.toJson(dogs);
+		resp.getWriter().print("{ \"query\": \"" + dogs.size() + "\", \"dogs\": "+ jsonDogsString + " }");;
+		resp.getWriter().flush();
+	}
+	
+}
