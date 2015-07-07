4b14be0ed79acb9a3b04be5e3a0375a6e9cbfb8f
vdarre <valentindarre@Valentins-MacBook-Air.local>
2015-07-07 13:19:29 -0400

    fixed location variable
    the location variable on the tests was in the wrong order, fixed to
    actually match model

--- src/test/java/model/DogTest.java
+++ src/test/java/model/DogTest.java
@@ -14,14 +14,15 @@ public class DogTest {
 	 */
 	@Test
 	public void dogsWithDifferentIdTest() {
-		Dog dog1 = new Dog("00000001","white plains", "lucky", "altered", "male", "boston terrier", "red and white");
-		Dog dog2 = new Dog("00000002","white plains", "lucky", "altered", "male", "boston terrier", "red and white");	
+		Dog dog1 = new Dog("00000001", "lucky", "altered", "male", "boston terrier", "red and white", "white plains");
+		Dog dog2 = new Dog("00000002", "lucky", "altered", "male", "boston terrier", "red and white", "white plains");	
 		assertFalse(dog1.equals(dog2));
 	}		
 	
 	@Test
 	public void dogNullName(){
-		Dog dog = new Dog("00000001","white plains", null, "altered", "male", "boston terrier", "red and white");
+		Dog dog = new Dog("00000001", null, "altered", "male", "boston terrier", "red and white", "white plains");
+		System.out.println(dog.getName());
 		assertNull(dog.getName());
 	}
 }
